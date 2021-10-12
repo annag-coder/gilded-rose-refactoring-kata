a9075d2f778b0b946f5c7113f786c2fb2907a436
Anna Gladushko <anna.gladushko@hapara.com>
2021-10-12 10:39:51 -0400

    Added test coverage for sellIn property

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,12 +1,53 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
-describe('Gilded Rose', function () {
+const ITEM_NAMES = {
+    GENERAL: '+5 Dexterity Vest',
+    AGED: 'Aged Brie',
+    SULFURAS: 'Sulfuras, Hand of Ragnaros',
+    BACKSTAGE: 'Backstage passes to a TAFKAL80ETC concert',
+}
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+const generalSellInCheck = itemName => it('should decrease sellIn by 1', function() {
+    const gildedRose = new GildedRose([ new Item(itemName, 3, 20) ]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(2);
+    items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(1);
+    items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(0);
+    items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(-1);
+    items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(-2);
+});
+
+const noChangeSellInCheck = itemName => it('should never decrease sellIn', function() {
+    const gildedRose = new GildedRose([ new Item(itemName, 3, 20) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(3);
+});
+
+describe('Gilded Rose: updateQuality', function () {
+    describe('General item', function () {
+        generalSellInCheck(ITEM_NAMES.GENERAL)
+
+        it('should degrade quality by 1', function() {
+            const gildedRose = new GildedRose([ new Item(ITEM_NAMES.GENERAL, 3, 20) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).to.equal(19);
+        });
     });
 
+    describe('Aged item', function () {
+        generalSellInCheck(ITEM_NAMES.AGED)
+    });
+
+    describe('Backstage item', function () {
+        generalSellInCheck(ITEM_NAMES.BACKSTAGE)
+    });
+
+    describe('Sulfuras item', function () {
+        noChangeSellInCheck(ITEM_NAMES.SULFURAS)
+    });
 });
